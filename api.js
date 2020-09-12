6ff4119381dff56c17b5737b81e885008e7515fb
undefined <masnn0@outlook.com>
2020-09-12 23:41:33 +0800

    f

--- api.js
+++ api.js
@@ -1,49 +1,24 @@
-const http = require('http');
-const url = require('url');
+const body = require('koa-body');
+const Koa = require('koa');
 
 const hostname = '127.0.0.1';
-const postport = 3000;
-const getport = 3001;
-
+const port = 3001;
 var a = [];
 
-const postserver = http.createServer((req, res) => {
-  var q = url.parse(req.url, true).query;
-  var ip = req.headers["x-real-ip"];
+const app = new Koa();
+app.use(body());
+app.use((ctx) => {
+  const ip = ctx.request.headers["x-real-ip"] || ctx.request.headers['x-forwarded-for'] || ctx.request.ip;
   if (ip) {
     ip = ip.split(".");
     ip[0] = "**";
-    if (ip.length > 3) {
-      ip[3] = "**";
-    }
+    if (ip.length > 3) ip[3] = "**";
     ip = ip.join(".");
-  }
-  else {
-    ip = "";
-  }
+  } else ip = "";
   q.ip = ip;
-  if (q.title || q.sid) {
-    a.push(q);
-  }
-  if (a.length > 16) {
-    a.shift();
-  }
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("");
-});
+  if (q.title || q.sid) a.push(q);
+  if (a.length > 16) a.shift();
+  ctx.body = JSON.stringify(a);
+})
 
-const getserver = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(JSON.stringify(a));
-});
-
-postserver.listen(postport, hostname, () => {
-  console.log(`Server running at http://${hostname}:${postport}/`);
-});
-getserver.listen(getport, hostname, () => {
-  console.log(`Server running at http://${hostname}:${getport}/`);
-});
-
+app.listen(port, hostname);
